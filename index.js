e44048ee7c205f32a005cbe572d00b5831e331f0
Daniel Noworyta <daniel.noworyta@gmail.com>
2020-08-27 09:39:30 +0200

    Added callbacks for outside world

--- index.js
+++ index.js
@@ -1,21 +1,50 @@
 class Timer {
-  constructor(durationInput, startButton, pauseButton) {
+  constructor(durationInput, startButton, pauseButton, callbacks) {
     this.durationInput = durationInput;
     this.startButton = startButton;
     this.pauseButton = pauseButton;
+    if (callbacks) {
+      this.onStart = callbacks.onStart;
+      this.onTick = callbacks.onTick;
+      this.onComplete = callbacks.onComplete;
+    }
 
-    startButton.addEventListener("click", this.start.bind(this));
-    pauseButton.addEventListener("click", this.pause.bind(this));
+    startButton.addEventListener("click", this.start);
+    pauseButton.addEventListener("click", this.pause);
   }
 
-  start() {
-    let interval = setInterval(function () {
-      durationInput.value = Number(durationInput.value) - 1;
-    }, 1000);
-    this.interval = interval;
-  }
-  pause() {
+  start = () => {
+    if (this.onStart) {
+      this.onStart();
+    }
+
+    this.tick();
+    this.interval = setInterval(this.tick, 1000);
+  };
+  pause = () => {
     clearInterval(this.interval);
+  };
+
+  tick = () => {
+    if (this.timeRemaining <= 0) {
+      this.pause();
+      if (this.onComplete) {
+        this.onComplete();
+      }
+    } else {
+      this.timeRemaining = this.timeRemaining - 1;
+      if (this.onTick) {
+        this.onTick();
+      }
+    }
+  };
+
+  get timeRemaining() {
+    return parseFloat(this.durationInput.value);
+  }
+
+  set timeRemaining(time) {
+    this.durationInput.value = time;
   }
 }
 
@@ -23,4 +52,14 @@ const durationInput = document.querySelector("#duration");
 const startButton = document.querySelector("#start");
 const pauseButton = document.querySelector("#pause");
 
-const timer = new Timer(durationInput, startButton, pauseButton);
+const timer = new Timer(durationInput, startButton, pauseButton, {
+  onStart() {
+    console.log("timer started");
+  },
+  onTick() {
+    console.log("TIK TAK");
+  },
+  onComplete() {
+    console.log("timer completed");
+  },
+});
